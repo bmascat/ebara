394b489e49359c017549abbde82b28b83aa03026
Charco <b.mascat@gmail.com>
2025-02-12 20:51:09 +0100

    fix(pubmed_retriever): correct class name in init.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,4 @@
 from model_manager import ModelManager
-from pubmed_retriever import Retriever
+from pubmed_retriever import PubMedRetriever
 from embedding_processor import EmbeddingProcessor
 from database_manager import DatabaseManager
