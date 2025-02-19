4c7cfd8963a8321df574ebbd24b54fa863053e65
Brais <b.mascat@gmail.com>
2025-02-19 16:45:12 +0100

    refactor(retrievers): implement baseretriever in pubmedretriever

--- src/models/retrievers/pubmed_retriever.py
+++ src/models/retrievers/pubmed_retriever.py
@@ -1,19 +1,26 @@
 from pymed import PubMed
+from .base_retriever import BaseRetriever
+from typing import List
 
-class PubMedRetriever:
+class PubMedRetriever(BaseRetriever):
     """
-    Clase para manejar la consulta a PubMed y obtener los artículos relevantes.
+    Retriever para la base de datos PubMed.
     Utiliza PyMed para conectarse a la API de PubMed.
     """
 
-    def __init__(self, max_results=20):
+    def __init__(self, max_results: int = 20):
+        super().__init__(max_results)
         self.pubmed = PubMed()
-        self.max_results = max_results
 
-    def fetch_articles(self, query: str) -> list:
+    def fetch_articles(self, query: str) -> List[str]:
         """
-        Obtiene los primeros 'max_results' artículos de PMC según la consulta.
-        Devuelve una lista de abstracts de los artículos.
+        Obtiene los primeros 'max_results' artículos de PubMed según la consulta.
+        
+        Args:
+            query (str): Query de búsqueda
+            
+        Returns:
+            List[str]: Lista de abstracts de los artículos
         """
         results = self.pubmed.query(query, max_results=self.max_results)
         abstracts = []
